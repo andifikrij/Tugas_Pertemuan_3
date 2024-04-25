c6eadb962aa02c6ee9f8da1f19d6f794aebbb7ac
andifikrij <andifikrijulianto@gmail.com>
2024-04-25 17:07:40 +0700

    menambahkan file baru untuk tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Andi Fikri</li>
+ <li>Nama Belakang : Julianto</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Alamanda</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Berenang</li>
+ <li>Bulutangkis</li>
+ </ul>
+</section>
